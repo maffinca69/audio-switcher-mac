8fd2b7060ef12e29c185621ae823d9fab3d1b2f1
Vyacheslav Bazhenov <maffinca69@gmail.com>
2023-02-08 16:12:30 +0400

    Initial Commit

---
+++ AudioInputSwitcher/AudioInputSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioInputSwitcherApp.swift
+//  AudioInputSwitcher
+//
+//  Created by  Jolly on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AudioInputSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
